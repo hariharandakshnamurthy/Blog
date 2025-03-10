3528e7857d2360b64f4b319ea048174d157e2057
Hari haran <hariharandakshnamurthy@gmail.com>
2025-03-10 23:54:50 +0530

    deleted duplicate folders

--- blog/blog_project/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Category, Post, Comment, Like
-
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
